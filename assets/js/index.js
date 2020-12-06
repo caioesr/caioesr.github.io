294916794e0463194a5f8777e945e991ba15736d
caioesr <caioesr@al.insper.edu.br>
2020-12-06 10:09:45 -0300

    colocando template básico

--- assets/js/index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  let db = coDesConnect('https://projeto-codes-2020-1.firebaseio.com/')
-
-  db.download('/', function(data) {
-    context = data
-    coDesReplace('.nav-buttons', context)
-    coDesReplace('.categorias',context)
-  })
-})
